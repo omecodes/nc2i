9f7565b93785e1cde78311517037ed3867608a7a
omecodes <omecodes@gmail.com>
2020-12-12 16:59:56 +0100

    Added filter for visit count

--- nc2i/middleware.go
+++ nc2i/middleware.go
@@ -6,8 +6,28 @@ import (
 	"github.com/omecodes/common/httpx"
 	"github.com/omecodes/common/utils/log"
 	"net/http"
+	"path"
 	"time"
 )
+
+var visitsExcludedExtensions = []string{
+	".js",
+	".css",
+	".ttf",
+	".otf",
+	".json",
+	".csh",
+	".svg",
+	".png",
+	".ico",
+}
+var visitsExcludedBaseNames = []string{
+	"NOTICES",
+	"robot.txt",
+}
+var visitsExcludedPaths = []string{
+	"/",
+}
 
 type middleware func(handler http.Handler) http.Handler
 
@@ -16,13 +36,35 @@ var logHandler = httpx.Logger("nc2i")
 func visits(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		handler.ServeHTTP(w, r)
+
+		agent := r.Header.Get("User-Agent")
+		remoteIP := r.Header.Get("Remote-Addr")
+		page := r.URL.Path
+
+		for _, x := range visitsExcludedPaths {
+			if r.URL.Path == x {
+				return
+			}
+		}
+
+		extension := path.Ext(r.URL.Path)
+		for _, x := range visitsExcludedExtensions {
+			if extension == x {
+				return
+			}
+		}
+
+		base := path.Base(r.URL.Path)
+		for _, x := range visitsExcludedBaseNames {
+			if base == x {
+				return
+			}
+		}
+
 		date := time.Now().UnixNano()
 		ctx := r.Context()
 		store := visitsInfoStore(ctx)
 		if store != nil {
-			agent := r.Header.Get("User-Agent")
-			remoteIP := r.Header.Get("Remote-Addr")
-			page := r.URL.Path
 
 			info := map[string]interface{}{
 				"date":  date,
