09df28a43ad6add7f46fbdb91094be38b096dd8e
omecodes <omecodes@gmail.com>
2021-02-03 15:17:02 +0100

    Implemented web page drawer

--- www/app/js/script.js
+++ www/app/js/script.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+
+function sidebarToggle() {
+    document.getElementById('side-bar').classList.toggle('active');
+    document.getElementById('side-bar-container').classList.toggle('active');
+}
+
+function sidebarClickOutside() {
+    document.getElementById('side-bar').classList.toggle('active');
+    document.getElementById('side-bar-container').classList.toggle('active');
+}
+
+function initDrawer() {
+    let clickCatcher = document.getElementById('click-catcher');
+    if (clickCatcher != null) {
+        clickCatcher.onclick = sidebarClickOutside;
+    }
+}
+
+initDrawer();
